26211136d4fc984ef8fa3d3a1dca8126fd48d5a4
GeoNnim <yy4338@gamil.com>
2024-08-01 18:04:01 +0900

    240801End

--- src/components/ModeCtrl.jsx
+++ src/components/ModeCtrl.jsx
@@ -3,15 +3,16 @@ import React, { useState } from 'react';
 
 const ModeCtrl = () => {
   const [darkMode, setDarkMode] = useState(true);
-  //  const toggleTheme = () => {
-  //   return (
-  //     setDarkMode(!darkMode)
-  //   )}
+  // const toggleTheme = () => {
+  //   return setDarkMode(!darkMode);
+  // };
+
+  // return <button onClick={toggleTheme}>{darkMode ? <Moon /> : <Sun />}</button>;
+  //위나 아래 방식을 사용
   return (
     <button onClick={() => setDarkMode(!darkMode)}>
       {darkMode ? <Moon /> : <Sun />}
     </button>
   );
 };
-
 export default ModeCtrl;
