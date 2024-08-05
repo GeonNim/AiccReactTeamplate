75ab81f9e4c53eec0d0c2b5363f1fa3343a823b5
GeoNnim <yy4338@gamil.com>
2024-08-05 12:43:06 +0900

    ddd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Features from './components/Features';
 import Navbar from './components/Navbar';
@@ -9,18 +9,21 @@ import ScrollToTop from 'react-scroll-to-top';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/AiccReactTeamplate">
       <div className="App">
         <Navbar />
 
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/features" element={<Features />} />
           {/* path: URL경로 정의, element: 렌더링될 컴포넌트 정의  */}
           {/* path의 이름 정의 가능 */}
-
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
+          {/* Default route to Home component */}
+          <Route path="/" element={<Navigate to="/home" />} />
+          {/* Catch all unmatched routes and redirect to Home */}
+          <Route path="*" element={<Navigate to="/home" />} />
         </Routes>
         <Footer />
         <ScrollToTop
